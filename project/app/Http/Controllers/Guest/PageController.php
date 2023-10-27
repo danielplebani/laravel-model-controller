2207cdc0558ff5b46bde1eeade5262e69877d0f3
danielplebani <danielpleba03@gmail.com>
2023-10-27 16:00:43 +0200

    add structure
    with PageController,Model.
    Create a database and import a Movies Table

---
+++ project/app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return view('main', ['Movie' => Movie::all()]);
+    }
+}
